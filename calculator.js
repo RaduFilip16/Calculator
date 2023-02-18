51325749de9b128e1ba68bc1d2bfd9416e218923
Filip Radu <filip16radu.m@gmail.com>
2023-02-18 12:00:44 +0200

    Add simple calculator functions

---
+++ calculator.js
@@ -0,0 +1,20 @@
+function add(number1, number2){
+    return number1 + number2;
+}
+
+function subtract(number1, number2){
+    return number1 - number2;
+}
+
+function multiply(number1, number2){
+    return number1 * number2;
+}
+
+function divide(number3, number4){
+    return (number3 / number4);
+}
+
+console.log(add(0,1));
+console.log(subtract(10,7));
+console.log(multiply(8,2));
+console.log(divide(10,5));

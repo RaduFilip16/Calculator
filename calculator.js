e3f154bafd5b01b601bd4e524de86cc7f8d85f23
Filip Radu <filip16radu.m@gmail.com>
2023-03-31 19:49:46 +0300

    Add DOM elements and event listeners

--- calculator.js
+++ calculator.js
@@ -1,3 +1,33 @@
+
+// Variables for HTML file
+
+const number_buttons = document.querySelectorAll("div > button");
+display = document.querySelector(".display");
+
+
+/*
+function show(element){
+    const display = document.querySelector(".display");
+    console.log(element.textContent);
+    display.innerHTML = "";
+    text = element.textContent;
+    display.innerHTML = text;
+}
+*/
+number_buttons.forEach(element => {
+    element.addEventListener("click", function(){
+        console.log('apasat');
+        display.innerHTML = "";
+        text = element.textContent;
+        console.log(text);
+        display.innerHTML = text;
+
+
+    });
+});
+
+
+
 function add(number1, number2){
     return number1 + number2;
 }
@@ -13,6 +43,7 @@ function multiply(number1, number2){
 function divide(number3, number4){
     return (number3 / number4);
 }
+
 
 function operate(operator, number1, number2){
     switch (operator){
